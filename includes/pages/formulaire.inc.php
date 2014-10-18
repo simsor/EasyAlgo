f31054431db3becf4ee91dfafefd42d5e8e2e008
simoncourtin <courtin.simon@gmail.com>
2014-10-18 12:18:05 +0200

    Formulaire liste langage

--- includes/pages/formulaire.inc.php
+++ includes/pages/formulaire.inc.php
@@ -1,13 +1,26 @@
 
 <form action="">
-    <textarea name="cartouche" id="cartouche">
+    <h3>Cartouche</h3>
+    <textarea cols="40" rows="15" name="cartouche" id="cartouche">
         Tapez votre cartouche    
     </textarea>
-    <textarea name="algo" id="algo">
+    
+    <h3>Algo</h3>
+    <textarea cols="40" rows="15" name="algo" id="algo">
         Tapez votre code Algo
     </textarea>
-    <textarea name="code" id="code">
+    <h3>Code</h3>
+    <textarea cols="40" rows="15" name="code" id="code">
         Tapez votre code
     </textarea>
-    <input type="submit" id="generate" name="generate"/>
+    <br>
+    <label for="langage">Langage de programmation</label>
+    <select id="langage" name="language">
+        <option value="c">C</option>
+        <option value="cpp">C++</option>
+        <option value="py">Python</option>
+        <option value="java">Java</option>
+    </select>
+    <br>
+    <input type="submit" id="generate" name="generate">
 </form>
