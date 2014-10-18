5b8f4ba1d9eba7cc4cabd0ffa0e36151e7151d20
ThibaultTricard <admin@Mac.mshome.net>
2014-10-18 12:29:10 +0200

    header v1.0

--- includes/header.inc.php
+++ includes/header.inc.php
@@ -1,4 +1,14 @@
-     <nav class="navbar navbar-inverse">
-        <ul class="nav navbar-nav">
-        </ul>
-      </nav>
+      <div class="container">
+        <nav class="navbar navbar-inverse">
+          <div class="col-sm-12 col-lg-7">
+            <div class="navbar-header">
+              <a class="navbar-brand" href="#">EasyAlgo</a>
+            </div>
+            <ul class="nav navbar-nav" >
+                <li><a href="#">Contact</a></li>
+                <li><a href="#">Dons</a></li>
+            </ul>
+          </div>
+        </nav>
+      </div>
+      
