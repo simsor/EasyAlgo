447a32c46bfd9a3511262284c0159a103c67b2ee
Simon Garrelou <simon.garrelou@riseup.net>
2014-10-18 19:46:14 +0200

    MàJ footer

--- includes/footer.inc.php
+++ includes/footer.inc.php
@@ -3,7 +3,7 @@
 				<div >
       				<div class="container">
         				<p class="text-muted credit">Réalisé par Courtin, Garrelou, Jarrige, Imbert, Tricard</p>
-        				<p class="text-muted credit">Avec PhP, Boutstrap, Aptana, Sublime Text 2, JQuery</p>
+        				<p class="text-muted credit">Avec PHP, Bootstrap, Aptana, Sublime Text 2, Emacs et jQuery</p>
         				<p class="text-muted credit">Dans le cadre du Limoges Hack 2014© organisé par l'association de l'OEIL (Organisation des Etudiants en Informatique de Limoges) </p>
       				</div>
 				</div>
