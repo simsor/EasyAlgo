1e51e0d7af8148a7629a4f26c5d34bcddb2d5df3
Simon Garrelou <simon.garrelou@riseup.net>
2014-10-18 19:16:57 +0200

    Amélioration du header

--- includes/header.inc.php
+++ includes/header.inc.php
@@ -1,14 +1,13 @@
-      <div class="container">
-        <nav class="navbar navbar-inverse">
-          <div class="col-sm-12 col-lg-7">
-            <div class="navbar-header">
-              <a class="navbar-brand" href="index.php">EasyAlgo</a>
-            </div>
-            <ul class="nav navbar-nav" >
-                <li><a href="index.php?id=1">Syntaxeur</a></li>
-                <li><a href="index.php?id=2">Contact</a></li>
-            </ul>
-          </div>
-        </nav>
-      </div>
-      
+<div class="navbar navbar-inverse" role="navigation">
+  <div class="container">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="index.php">EasyAlgo</a>
+    </div>
+    <div class="collapse navbar-collapse">
+      <ul class="nav navbar-nav" >
+	<li><a href="index.php?id=1">Syntaxeur</a></li>
+	<li><a href="index.php?id=2">Contact</a></li>
+      </ul>
+    </div>
+  </div>
+</div>
