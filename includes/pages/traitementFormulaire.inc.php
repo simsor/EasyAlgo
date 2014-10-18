c3b6a990592be4daddc5019d7b4021df8188450e
Mardaspan <imbert.kevin94@gmail.com>
2014-10-18 20:39:23 +0200

    MaJ traitement Formulaire

--- includes/pages/traitementFormulaire.inc.php
+++ includes/pages/traitementFormulaire.inc.php
@@ -5,75 +5,82 @@ if(!empty($_POST['type']))
 {
     $type=$_POST['type'];
 }
+
+
 switch($type)
 {
     case "procedure" :
-        
-        
+		$description=$_POST['definition'];
+		//on récupère les entrées
+		if(!empty($_POST['Entree'.$i]))
+		{
+   			while(!empty($_POST['Entree'.$i]))
+    		{
+           		$entrees[]=$_POST['Entree'.$i];
+     		}
+		}
+		//on récupère les prè-conditions
+		if(!empty($_POST['precondition'.$i]))
+		{
+   			while(!empty($_POST['precondition'.$i]))
+   			{
+          		$preconditions[]=$_POST['precondition'.$i];
+    		}
+		}
+		
+		$i=1;
+    	//on récupère les sorties
+    	while(!empty($_POST['Sortie'.$i]))
+    	{
+        	$sortie[] = $_POST['Sortie'.$i];
+    	}
+    	$i=1;
+    	//on recupère les post-conditions
+    	while(!empty($_POST['postcondition'.$i]))
+    	{
+        	$postcondition[]=$_POST['postcondition'.$i];
+    	}
+		$procedure = new Procedure($description, $entrees, $preconditions, $sortie, $postcondition);
+		
         break;
     case "fonction" :
-        
+		$description=$_POST['definition'];
+		//on récupère les entrées
+		if(!empty($_POST['Entree'.$i]))
+		{
+    		while(!empty($_POST['Entree'.$i]))
+    		{
+           		$entrees[]=$_POST['Entree'.$i];
+     		}
+		}
+		//on récupère les prè-conditions
+		if(!empty($_POST['precondition'.$i]))
+		{
+  			while(!empty($_POST['precondition'.$i]))
+   			{
+          		$preconditions[]=$_POST['precondition'.$i];
+    		}
+		}
+		
+		$i=1;
+    	//on va chercher le retourne (unique)
+    	while(!empty($_POST['Sortie1']))
+    	{
+        	$retourne =$_POST['Sortie1'];
+    	}
+		$fonction = new Fonction($description, $entrees, $preconditions, $retourne);
         
         break;
     case "dejacode" :
-        
-        
+	    if(!empty($_POST['cartouche']))
+    		$cartouche = $_POST['cartouche'];
+		
+		echo $cartouche;
         break;
     default :
-        
+        echo "Erreur type formulaire ! \n";
         break;
 }
-
-function affecteRenseignBase()
-{
-    $decription=$_POST['definition'];
-    //on récupère les entrées
-    if(!empty($_POST['Entree'.$i]))
-    {
-        while(!empty($_POST['Entree'.$i]))
-        {
-            $entrees[]=$_POST['Entree'.$i];
-        }
-    }
-    //on récupère les prè-conditions
-    if(!empty($_POST['precondition'.$i]))
-    {
-        while(!empty($_POST['precondition'.$i]))
-        {
-            $preconditions[]=$_POST['precondition'.$i];
-        }
-    }    
-}
-
-function affecteProcedure()
-{
-    $i=1;
-    //on récupère les sorties
-    while(!empty($_POST['Sortie'.$i]))
-    {
-        $sortie[] = $_POST['Sortie'.$i];
-    }
-    $i=1;
-    //on recupère les post-conditions
-    while(!empty($_POST['postcondition'.$i]))
-    {
-        $postcondition[]=$_POST['postcondition'.$i];
-    }
-}
-function affecteFunction()
-{
-    $i=1;
-    //on va chercher le retourne (unique)
-    while(!empty($_POST['Sortie1']))
-    {
-        $retourne =$_POST['Sortie1'];
-    }
-}
-
-
-
-if(!empty($_POST['cartouche']))
-    $cartouche = $_POST['cartouche'];
 
 if(!empty($_POST['algo']))
     $algo= $_POST['algo'];
