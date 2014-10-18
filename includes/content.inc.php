1de451fea720749abea1fe94be1b6e7b7c328fae
Simon Garrelou <simon.garrelou@riseup.net>
2014-10-18 12:59:22 +0200

    Ajout classe container au contenu

--- includes/content.inc.php
+++ includes/content.inc.php
@@ -1,26 +1,28 @@
-<?php
+<div class="container">
+  <?php
 
-if (!isset($_GET['id']))
-{
-  require "pages/index.inc.php";
-}
-else
-{
-  $page = $_GET['id'];
-  switch($page)
-    {
-    case 0:
-      require "includes/pages/index.inc.php";
-      break;
+  if (!isset($_GET['id']))
+  {
+    require "pages/index.inc.php";
+  }
+  else
+  {
+    $page = $_GET['id'];
+    switch($page)
+      {
+      case 0:
+	require "includes/pages/index.inc.php";
+	break;
 
-    case 1:
-      require "includes/pages/formulaire.inc.php";
-      break;
+      case 1:
+	require "includes/pages/formulaire.inc.php";
+	break;
 
-    default:
-      require "includes/pages/404.inc.php";
-      break;
-    }
-}
+      default:
+	require "includes/pages/404.inc.php";
+	break;
+      }
+  }
 
-?>
+  ?>
+</div>
