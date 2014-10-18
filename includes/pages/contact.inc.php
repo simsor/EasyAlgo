16d8d3fa11f332683ddd54489c03de0917bd6d2b
Simon Garrelou <simon.garrelou@riseup.net>
2014-10-18 19:04:36 +0200

    Design page contact fini. Maintenant faut que ça l'envoie.

--- includes/pages/contact.inc.php
+++ includes/pages/contact.inc.php
@@ -1,25 +1,42 @@
 <?php
-if (!isset($_POST['']))
+if (!isset($_POST['mail']))
+{
 ?>
 
-<form action="#" method="POST" role="form">
-  <div class="form-group col-lg-4">
-    Votre nom :
-    <input type="text" name="nom" class="form-control" />
+  <h2 class="col-lg-4 col-lg-offset-4">Donnez-nous vos impressions !</h2>
+
+  <form action="#" method="POST" role="form" class="col-lg-4 col-lg-offset-4">
+    <div class="form-group">
+      Votre nom :
+      <input type="text" name="nom" class="form-control" />
+    </div>
+
+    <div class="form-group">
+      Votre prénom :
+      <input type="text" name="prenom" class="form-control" />
+    </div>
+
+    <div class="form-group">
+      Votre adresse e-mail :
+      <input type="mail" name="mail" class="form-control" />
+    </div>
+
+    <div class="form-group">
+      Message :
+      <textarea name="message" placeholder="Vos mots d'amour" class="form-control"></textarea>
+    </div>
+
+    <input type="submit" class="btn btn-success col-lg-4">
+  </form>
+<?php
+}
+else
+{ ?>
+
+  <div class="col-lg-4 col-lg-offset-4">
+    <p>Merci pour votre feedback !</p>
   </div>
 
-  <div class="form-group col-lg-4">
-    Votre prénom :
-    <input type="text" name="prenom" class="form-control" />
-  </div>
-
-  <div class="form-group col-lg-4">
-    Votre adresse e-mail :
-    <input type="mail" name="mail" class="form-control" />
-  </div>
-
-  <div class="form-group col-lg-4">
-    Message :
-    <textarea name="message" placeholder="Vos mots d'amour" class="form-control"></textarea>
-  </div>
-</form>
+<?php
+}
+?>
