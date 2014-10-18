a5237b77a73295bde569e0c3265369cef689e4be
clem19s <clemj1900@gamil.com>
2014-10-18 12:43:01 +0200

    ajout id container 404.inc.php

--- includes/pages/404.inc.php
+++ includes/pages/404.inc.php
@@ -1,3 +1,6 @@
-    <h1>Error 404 : Not found</h1>
-    <p>La page que vous avez demandée n'existe pas !'</p>
-    <p><a href="index.php">Retour à l'accueil</a></p>
+    <div id="container">
+        <h1>Error 404 : Not found</h1>
+        <p>La page que vous avez demandée n'existe pas !'</p>
+        <p><a href="index.php">Retour à l'accueil</a></p>
+    </div>
+    
