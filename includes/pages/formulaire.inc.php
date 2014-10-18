083ceb3869a35626f6e240ab9551d37aa13eceb5
simoncourtin <courtin.simon@gmail.com>
2014-10-18 12:20:12 +0200

    Revert "Formulaire liste langage"
    This reverts commit f31054431db3becf4ee91dfafefd42d5e8e2e008.

--- includes/pages/formulaire.inc.php
+++ includes/pages/formulaire.inc.php
@@ -1,26 +1,13 @@
 
 <form action="">
-    <h3>Cartouche</h3>
-    <textarea cols="40" rows="15" name="cartouche" id="cartouche">
+    <textarea name="cartouche" id="cartouche">
         Tapez votre cartouche    
     </textarea>
-    
-    <h3>Algo</h3>
-    <textarea cols="40" rows="15" name="algo" id="algo">
+    <textarea name="algo" id="algo">
         Tapez votre code Algo
     </textarea>
-    <h3>Code</h3>
-    <textarea cols="40" rows="15" name="code" id="code">
+    <textarea name="code" id="code">
         Tapez votre code
     </textarea>
-    <br>
-    <label for="langage">Langage de programmation</label>
-    <select id="langage" name="language">
-        <option value="c">C</option>
-        <option value="cpp">C++</option>
-        <option value="py">Python</option>
-        <option value="java">Java</option>
-    </select>
-    <br>
-    <input type="submit" id="generate" name="generate">
+    <input type="submit" id="generate" name="generate"/>
 </form>
