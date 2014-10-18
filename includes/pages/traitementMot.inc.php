29ecb7646eba30635f5d361ab8dd37cd5e977f3a
simoncourtin <courtin.simon@gmail.com>
2014-10-18 17:27:18 +0200

    Fonction gras surlignage qui marche

--- includes/pages/traitementMot.inc.php
+++ includes/pages/traitementMot.inc.php
@@ -10,14 +10,32 @@ if(isset($_POST['valider']))
     include '../../classes/Interpreteur.class.php';
     $interpreteur= new Interpreteur();
     $chaine = $_POST['chaine'];
-    $chaine = explode(" ", $chaine);
-    foreach($chaine as $value)
+    ?><pre><code><?php
+    $chaineTmp="";
+    for($i=0;$i<strlen($chaine);$i++)
+    {
+        if(($chaine[$i]>='a' and $chaine[$i]<='z')or($chaine[$i]>='A' and $chaine[$i]<='Z')or($chaine[$i]>='à' and $chaine[$i]<='ü') )
+        {
+            $chaineTmp.=$chaine[$i];
+        }
+        else
+        {
+            if($interpreteur->correspondance($chaineTmp))
+                echo "<b><u>$chaineTmp</u></b> ";
+            else
+                echo $chaineTmp;
+            $chaineTmp="";
+            echo $chaine[$i];
+        }
+    }
+    ?></code></pre>     <?php
+    /*foreach($chaine as $value)
     {
         if($interpreteur->correspondance($value))
             echo "<b><u>$value</u></b> ";
         else
-            echo $value; 
-    }
+            echo $value;
+    }*/
 }
 ?>
 <form action="#" method="POST">
