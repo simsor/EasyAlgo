fd60f330394b048fe55b8bfccc3c3c50f12c99aa
clem19s <clemj1900@gamil.com>
2014-10-18 14:19:25 +0200

    ajout lien index.php

--- includes/header.inc.php
+++ includes/header.inc.php
@@ -2,7 +2,7 @@
         <nav class="navbar navbar-inverse">
           <div class="col-sm-12 col-lg-7">
             <div class="navbar-header">
-              <a class="navbar-brand" href="#">EasyAlgo</a>
+              <a class="navbar-brand" href="index.php">EasyAlgo</a>
             </div>
             <ul class="nav navbar-nav" >
                 <li><a href="#">Contact</a></li>
