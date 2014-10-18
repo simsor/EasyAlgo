3fe89c4f53f5e8e733098bf40de87d70d07a1c27
Simon Garrelou <simon.garrelou@riseup.net>
2014-10-18 21:58:21 +0200

    Un box-shadow de fou !!

--- includes/footer.inc.php
+++ includes/footer.inc.php
@@ -1,22 +1,22 @@
 
-        <nav id="footer" style="background-color: #222222; padding-top: 7px;">
-				<div >
-      				<div class="container">
-        				<p class="text-muted credit">Réalisé par Courtin, Garrelou, Jarrige, Imbert, Tricard</p>
-        				<p class="text-muted credit">Avec PHP, Bootstrap, Aptana, Sublime Text 2, Emacs et jQuery</p>
-        				<p class="text-muted credit">Dans le cadre du Limoges Hack 2014© organisé par l'association de l'OEIL (Organisation des Etudiants en Informatique de Limoges) </p>
-      				</div>
-				</div>
-    	</nav>
+<div id="footer">
+  <div >
+    <div class="container">
+      <p class="text-muted credit">Réalisé par Courtin, Garrelou, Jarrige, Imbert, Tricard</p>
+      <p class="text-muted credit">Avec PHP, Bootstrap, Aptana, Sublime Text 2, Emacs et jQuery</p>
+      <p class="text-muted credit">Dans le cadre du Limoges Hack 2014© organisé par l'association de l'OEIL (Organisation des Etudiants en Informatique de Limoges) </p>
+    </div>
+  </div>
+</div>
 
 
 <script>
 
-$(function footer(){
-		if($(window).height() >= $("body").height()){
-			$("#footer").css({"position": "absolute","bottom": "0"});
-		}
-		$("#footer").css({"width":"100%","margin-bottom":"0px"});
-});
+ $(function footer(){
+   if($(window).height() >= $("body").height()){
+     $("#footer").css({"position": "absolute","bottom": "0"});
+   }
+   $("#footer").css({"width":"100%","margin-bottom":"0px"});
+ });
 
 </script>
